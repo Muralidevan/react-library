3c825d10077d1fa755ac75d628f6cdc76836eb11
Muralidevan <murali@theretailinsights.com>
2023-06-28 11:50:18 +0530

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+//common js modules, import json as a javascript variable
+const packageJson = require("./package.json");
+
+export default [
+  {
+    //entry point for library
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  //dts plugin config
+  {
+    input: "dist/esm/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
